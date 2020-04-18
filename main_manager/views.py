fbbfe650ec9233e276676e767153c34793a69aed
Rafael Karosuo <rafaelkarosuo@gmail.com>
2020-04-18 14:39:36 -0700

    Fix load static when debug without collectstatic using staticfiles_dirs setting, also added the url navigation for loading the shopping_list page when base url request

--- main_manager/views.py
+++ main_manager/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def shopping_list(request):
+
+    return render(request, 'main_manager/shopping_list.html', None)
+
---
+++ static/apps/main_manager/js/shopping_list_vue.js
@@ -0,0 +1,14 @@
+alert("hello");
+var recipe_manager = new Vue({
+    delimiters: ['[[', ']]'],
+    el: "#asn_manager_app",
+    vuetify: new Vuetify({
+        theme: {
+            dark: false
+        }
+    }),
+    data: {},
+    watch: {},
+    methods: {}
+
+});
