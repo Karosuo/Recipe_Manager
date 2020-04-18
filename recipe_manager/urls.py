77a91555869dd05798ec98e4ff5d5364ce5eebe7
Rafael Karosuo <rafaelkarosuo@gmail.com>
2020-04-18 13:09:38 -0700

    Fix project name reference on settings to start server

--- recipe_manager/urls.py
+++ recipe_manager/urls.py
@@ -1,4 +1,4 @@
-"""untitled URL Configuration
+"""recipe_manager URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
