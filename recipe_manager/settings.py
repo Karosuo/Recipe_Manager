dd69463c8caf8aaca810cd996c30a5b994094ece
Rafael Karosuo <rafaelkarosuo@gmail.com>
2020-06-22 19:35:06 -0700

    Added commented steps of empty function, to create the shopping list

--- recipe_manager/settings.py
+++ recipe_manager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c#x*4*a0esth0@dypbs&i@3xiyrv3t7@@m%pp-ep(_+dhp*l5y'
+SECRET_KEY = 'c#x*8*a0tesh0@dypbs&i@3xiyrv3t7@@m%pp-pe(_+dhp*l5y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
